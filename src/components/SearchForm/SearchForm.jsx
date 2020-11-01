86c8b28ec7e2ab2ff011e75fa16472a46fbc23ed
Yuri Zavyalov <to-umaster@mail.ru>
2020-11-01 23:29:01 +0600

    add dispatch

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import {useDispatch} from 'react-redux';
 
-const SearchForm = () => (
-  <>
+import {setUsername} from '../../slices/username.slice';
+import {fetchRepos} from '../../slices/repos.slice';
+
+
+const SearchForm = () => {
+  const dispatch = useDispatch();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(setUsername);
+  }
+
+  return (
     <form>
       <input type="text" name="username" id="username" placeholder="User name"></input>
     </form>
-  </>
-);
+  );
+};
 
 export default SearchForm;
