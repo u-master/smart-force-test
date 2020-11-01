8bef59275e864ebdee5f2d569ab8ddd67b1c40d8
Yuri Zavyalov <to-umaster@mail.ru>
2020-11-01 20:14:26 +0600

    set fetchrepos action

--- src/slices/repos.slice.js
+++ src/slices/repos.slice.js
@@ -1,6 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
+import {getReposPath} from '../routes';
 import { setUsername } from './username.slice';
+
+// const fetchRepos = () => (dispatch) => {
+  
+// };
+
+const fetchRepos  = createAsyncThunk(
+  'username/fetchRepos',
+  async (username) => await axios.get(getReposPath(username)));
+
+
 
 const { reducer, actions } = createSlice({
   name: 'repos',
@@ -9,24 +20,35 @@ const { reducer, actions } = createSlice({
     fetchingState: null,
     fetchingError: null,
   },
-  reducers: {
-    reposFetching: ({ fetchingState }) => {
+  // reducers: {
+  //   reposFetching: ({ fetchingState }) => {
+  //     fetchingState = 'fetching';
+  //   },
+  //   reposFetched: ({ fetchingState, data }, { payload: { repos } }) => {
+  //     data = repos;
+  //     fetchingState = 'success';
+  //   },
+  //   reposFetchingError: ({ fetchingState }, { payload: { error } }) => {
+  //     fetchingState = 'failed';
+  //     fetchingError = error;
+  //   },
+  // },
+  extraRedusers: {
+    [fetchRepos.fulfilled]: ({fetchingState, data}, {payload }) => {
+      data = payload;
+      fetchingState = 'success';
+      console.log(payload);
+    },
+    [fetchRepos.pending]: ({fetchingState}) => {
       fetchingState = 'fetching';
     },
-    reposFetched: ({ fetchingState, data }, { payload: { repos } }) => {
-      data = repos;
-      fetchingState = 'success';
-    },
-    reposFetchingError: ({ fetchingState }, { payload: { error } }) => {
+    [fetchRepos.rejected]: ({fetchingState, fetchingError}, {payload}) => {
       fetchingState = 'failed';
-      fetchingError = error;
-    },
-  },
-  extraReducers: {
-    [setUsername](state, { payload: { username } }) {
-      return;
-    },
+      fetchingError = payload;
+      console.log(payload);
+    }
   },
 });
 
+export {fetchRepos};
 export default reducer;
