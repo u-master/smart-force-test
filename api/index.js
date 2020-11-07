c74f38d065924ac06b804273a1c1707c4387809f
u-master <to-umaster@mail.ru>
2020-11-08 00:16:19 +0600

    fix auth

--- api/index.js
+++ api/index.js
@@ -16,8 +16,8 @@ app.get('/auth', (req, res) => {
     .post(
       `https://github.com/login/oauth/access_token?client_id=${client_id}&client_secret=${client_secret}&code=${code}`,
     )
-    .then((response) => res.send(response))
-    .catch((error) => res.send(error));
+    .then((response) => res.send(`Response: ${response}`))
+    .catch((error) => res.send(`Error: ${error}`));
 });
 
 export default app;
