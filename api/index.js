f7000aeaddb6493dbe10e5551f3147c556a422ce
u-master <to-umaster@mail.ru>
2020-11-08 00:03:37 +0600

    fix auth

--- api/index.js
+++ api/index.js
@@ -1,15 +1,22 @@
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
 
 app.get('/auth', (req, res) => {
   // const query = window.location.query;
   // const code = /^code=(\w*)$/g.exec(query);
+  const client_id = '4421486ad44ad061fbed';
+  const client_secret = '62da27c043a88ca97ea8bd5f621396a21e3ada87';
   const { code } = req.query;
-  const result = `<p>${code}</p>`;
-  // + (JSON.stringify(req) || 'null');
+  const result = `<p>Authentication...</p>
+  <p>Code: ${code}</p>`;
   res.send(result);
-  // <p>${window.location}</p> <p>${query}</p> <p>${code}</p> `);
+  axios
+    .post(
+      `https://github.com/login/oauth/access_token?client_id=${client_id}&client_secret=${client_secret}&code=${code}`,
+    )
+    .then((response) => res.send('Response!'));
 });
 
 export default app;
