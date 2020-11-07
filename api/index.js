9e0dfe162b5c36f8215c5be1e80acb8b1a01e76d
u-master <to-umaster@mail.ru>
2020-11-07 21:39:10 +0600

    fix auth

--- api/index.js
+++ api/index.js
@@ -3,7 +3,9 @@ import express from 'express';
 const app = express();
 
 app.get('/auth', (req, res) => {
-  console.log(req);
+  const query = window.location.query;
+  const code = /^code=(\w*)$/g.exec(query);
+  console.log(' >>> Auth', req, query, code);
   res.send('Auth!!!');
 });
 
