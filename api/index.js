ad3671c4d09abbef2aa8197b426c95e4feab8707
u-master <to-umaster@mail.ru>
2020-11-08 00:32:16 +0600

    fix auth

--- api/index.js
+++ api/index.js
@@ -9,14 +9,11 @@ app.get('/auth', (req, res) => {
   const client_id = '4421486ad44ad061fbed';
   const client_secret = '62da27c043a88ca97ea8bd5f621396a21e3ada87';
   const { code } = req.query;
-  // const result = `<p>Authentication...</p>
-  // <p>Code: ${code}</p>`;
-  // res.send(result);
   axios
     .post(
       `https://github.com/login/oauth/access_token?client_id=${client_id}&client_secret=${client_secret}&code=${code}`,
     )
-    .then((response) => res.send(response.data))
+    .then((response) => res.redirect(`/?${response.data}`))
     .catch((error) => res.send(`Error: ${error.name}`));
 });
 
