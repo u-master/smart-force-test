0ec672660c7bfe935f0f6c199fc2cb04b74a031e
u-master <yurgan@gmail.com>
2020-11-09 16:32:17 +0600

    fix backend in dev mode

--- api/index.js
+++ api/index.js
@@ -1,20 +1,32 @@
-import express from 'express';
-import axios from 'axios';
+const express = require('express');
+const axios = require('axios');
 
 const app = express();
 
+const authorize = (clientId, clientSecret, githubCode) => {
+  return axios.post(
+    `https://github.com/login/oauth/access_token?client_id=${clientId}&client_secret=${clientSecret}&code=${githubCode}`,
+  );
+};
+
 app.get('/auth', (req, res) => {
-  // const query = window.location.query;
-  // const code = /^code=(\w*)$/g.exec(query);
-  const client_id = '4421486ad44ad061fbed';
-  const client_secret = '62da27c043a88ca97ea8bd5f621396a21e3ada87';
+  const clientId = '4421486ad44ad061fbed';
+  const clientSecret = '62da27c043a88ca97ea8bd5f621396a21e3ada87';
   const { code } = req.query;
-  axios
-    .post(
-      `https://github.com/login/oauth/access_token?client_id=${client_id}&client_secret=${client_secret}&code=${code}`,
-    )
+  authorize(clientId, clientSecret, code)
     .then((response) => res.redirect(`/?${response.data}`))
     .catch((error) => res.send(`Error: ${error.name}`));
 });
 
-export default app;
+app.get('/authdev', (req, res) => {
+  const clientId = 'bd4f45a8fde0385a77a4';
+  const clientSecret = '3e08be1cc188b55f52a58753bad6a6eb0b232fbb';
+  const { code } = req.query;
+  authorize(clientId, clientSecret, code)
+    .then((response) => res.redirect(`http://localhost:3000/?${response.data}`))
+    .catch((error) => res.send(`Error: ${error.name}`));
+});
+
+app.listen(8080, () => console.log('Backend server listening on port 8080!'));
+
+module.exports = app;
